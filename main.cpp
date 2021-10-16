9fba07fc479458b9aceb333b50bbb595e66117e9
Raperprogramist <60778504+Raper-programista@users.noreply.github.com>
2021-10-16 08:44:07 +0000

    zmiany wizualne

--- main.cpp
+++ main.cpp
@@ -24,65 +24,45 @@ int main() {
 	int maxInt = 0;
 
 	//odczytaj plik, zapisz
-	for(int i=0; getline(plik, wiersz); i++)
+	for(int i=0; getline(plik, wiersz); i++){
 		list.push_back(wiersz);
+		//Wyświetli zawartość pliku ciągi.txt, po myślniku
+		cout << "- " << wiersz << endl;
+                        
+		//	iterowanie jednego wiersza                                              
+		int len = wiersz.length();
+		char char_array[len+1]; 							//deklaracja listy znaków          
+		strcpy(char_array, wiersz.c_str());  //inicjacja listy znaków                                                                                                                                              
+		for(int j=0; j<len; j++){                                        
+			                          //ilość 0 i 1                                                    ciągi składające się ze znaków 1	
+			if(char_array[j] == '0'){		count0++;                                                           same_jedynki = false;                  }
+																//                ciąg, w którym jest najwięcej znaków 1  
+			if(char_array[j] == '1'){   count1++;          count1_in_line++;                      } 
+			
+		}//for - iterowanie jednego wiersza
 
-	
-	for(int i=0; i<list.size(); i++){
+		                                          //Znajdzie ciąg, w którym jest najwięcej znaków 1
+		                                             if(count1_in_line > maxDlugosc){
+			                                          		maxDlugosc = count1_in_line;
+			                                              maxDlugosc_index = i;
+		                                             }
+	                                               	count1_in_line = 0;  
 
-		//Sprawdzi, czy są co najmniej dwa takie same ciągi
-		for(int j=i; j<list.size(); j++)//                |
-			if(list[i] == list[j] && i != j)//              |
-				duplikaty.push_back(list[i]);//_______________|
-//                        ___________________________________________________
-		//Iterowanie po bitach                                                   |
-		int len = list[i].length();//                                            |
-		char char_array[len+1]; 							//deklaracja listy znaków          |
-		strcpy(char_array, list[i].c_str());  //inicjacja listy znaków           |
-//                                                                           |
-					//Obliczy ilość 1 i 0 w całym pliku                                |
-		for(int j=0; j<len; j++)//							|                                |
-			if(char_array[j] == '0') count0++;//	|                                |
-			else count1++;//______________________|                                |
-//                                                                           |
-		//Znajdzie ciąg, w którym jest najwięcej znaków 1                        |
-		for(int j=0; j<len; j++)//                      |                        |
-			if(char_array[j] == '1') count1_in_line++;//  |                        |
-		//                                              |                        |
-		if(count1_in_line > maxDlugosc){//              |                        |
-			maxDlugosc = count1_in_line;//					 	 	  |                        |
-			maxDlugosc_index = i;//										 	  |                        |
-		}//																						  |                        |
-    //																						  |                        |
-		count1_in_line = 0;//___________________________|                        |
-//                                                                           |
-//		                                                                       |
-		//Wypisze wszystkie ciągi składające się wyłącznie ze znaków 1           |
-		for(int j=0; j<len; j++)//																	 |           |
-			if(char_array[j] == '0')//																 |           | 
-				same_jedynki = false;//																	 |           |
-		//																													 |           |
-		if(same_jedynki) jedynki.push_back(list[i]);//							 |           |
-		same_jedynki = true;//_______________________________________|           |
-		//		                                                                   |
-		//_______________________________________________________________________|
+																																																//Sprawdzi, czy są co najmniej dwa takie same ciągi
+																																																for(int j=0; j<list.size(); j++)
+																																																	if(wiersz == list[j] && i != j)
+																																																		duplikaty.push_back(wiersz);
+
+		//Wypisze wszystkie ciągi składające się wyłącznie ze znaków 1           
+		if(same_jedynki) jedynki.push_back(wiersz);//							            
+		same_jedynki = true;
 
 		//Jaką największą liczbę dziesiętną zapisano w pliku
-		try{	//																					 |
-		int liczba = stoi(list[i], 0, 2);//								 |
-		if(liczba > maxInt) maxInt = liczba;//						 |
-		}catch(...){}//____________________________________|
+		try{																				
+		int liczba = stoi(wiersz, 0, 2);							 
+		if(liczba > maxInt) maxInt = liczba;						 
+		}catch(...){}
 	}
-			
-	
-
-		
-
-
-
-	cout << "Wyświetli zawartość pliku ciągi.txt, po myślniku" << endl;
-		for(int i=0; i<list.size(); i++)
-			cout << "- " << list[i] << endl;
 
 	cout << endl << "Obliczy ilość 1 i 0 w całym pliku" << endl;
 		cout << "0: " << count0 << endl;
